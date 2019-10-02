e7719386c47e0e71ca3846ebf3d6c64dd8b793d9
fdortort <francis.dortort@illicus.com>
2019-10-02 04:00:19 -0400

    create a participant (agent conference)

--- Services/Twilio/Rest/Participants.php
+++ Services/Twilio/Rest/Participants.php
@@ -3,6 +3,15 @@
 class Services_Twilio_Rest_Participants
     extends Services_Twilio_ListResource
 {
+    public function create($from, $to, array $params = array())
+    {
+
+        $params["To"] = $to;
+        $params["From"] = $from;
+
+        return parent::_create($params);
+    }
+
     /* Participants are identified by CallSid, not like PI123 */
     public function getObjectFromJson($params, $idParam = "sid") {
         return parent::getObjectFromJson($params, "call_sid");
